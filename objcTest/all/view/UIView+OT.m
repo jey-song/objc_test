ff003a15dbcb5f53b5a2e17ad5c20161119d4c7d
jey <song.arain@gmail.com>
2016-06-12 17:02:32 +0800

    fix: ui event test

--- objcTest/all/view/UIView+OT.m
+++ objcTest/all/view/UIView+OT.m
@@ -26,20 +26,25 @@
 }
 
 - (nullable UIView *)mk_hitTest:(CGPoint)point withEvent:(nullable UIEvent *)event {
-//    if (self.tag == 200) {
-//        return self;
-//    }
+    NSLog(@"--view(%@:%p:%ld)", self.class, self, self.tag);
     UIView *v = [self mk_hitTest:point withEvent:event];
-    NSLog(@"view(%@:%p:%ld), hitTest:(%p) tag: %ld", self.class, self, self.tag, v, (long)v.tag);
+//    if (v.tag == 300) {
+//        v = nil;
+//    }
+//    if (self.tag == 301) {
+//        v = self;
+//    }
+    NSLog(@"==view(%@:%p:%ld), hitTest:(%p) tag: %ld", self.class, self, self.tag, v, (long)v.tag);
     return v;
 }
 
 - (BOOL)mk_pointInside:(CGPoint)point withEvent:(nullable UIEvent *)event {
+    NSLog(@"~~view(%@:%p:%ld)", self.class, self, self.tag);
     BOOL v = [self mk_pointInside:point withEvent:event];
 //    if (self.tag == 200) {
 //        v = NO;
 //    }
-    NSLog(@"view(%@:%p:%ld), %d", self.class, self, self.tag, v);
+    NSLog(@"==view(%@:%p:%ld), %d", self.class, self, self.tag, v);
     return v;
 }
 
